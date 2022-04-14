6a62fd200ea4f2cd505d040f215793b3a92f7fec
sinapradip <101270811+sinapradip@users.noreply.github.com>
2022-04-14 20:54:20 +0545

    files upload first

---
+++ index.js
@@ -0,0 +1,36 @@
+
+    
+// Reset game
+document.querySelector(".btn").addEventListener
+(
+    "click", function() 
+    {    
+        // Calculate random image name
+        var randomImageName1 = Math.floor(Math.random() * 6 + 1);
+        var randomImageName2 = Math.floor(Math.random() * 6 + 1);
+
+
+        //Javascript to make Dice game logic
+        var randomImageSource1 = "images/dice"+randomImageName1+".png";
+        
+        var randomImageSource2 = "images/dice"+randomImageName2+".png";
+        
+        document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+        
+        document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);   
+
+
+            // If player one wins
+            if (randomImageName1 > randomImageName2) {
+                document.querySelector("h1").innerHTML = "Player 1 Wins!";
+            }
+            // If player two winsAC
+            else if (randomImageName1 < randomImageName2) {
+                document.querySelector("h1").innerHTML = "Player 2 Wins!";
+            }
+            // If player one and two tie
+            else {
+                document.querySelector("h1").innerHTML = "Tie!";
+            }
+    }
+);
