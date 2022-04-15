3ca3cc5fb6c6d6352de79e68d3213669d56ef243
Nirajan Malla <nirajannitro5@gmail.com>
2022-04-15 22:25:19 +0545

    security + logic + more ...

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-
+let you= document.getElementById("you");
+let me= document.getElementById("me");
+let hide= document.getElementById("hide");
     
 // Reset game
 document.querySelector(".btn").addEventListener
 (
     "click", function() 
-    {    
+    {   
+        you.style.display="block";
+        me.style.display="block";
+        hide.style.display="inline";
+
         // Calculate random image name
-        var randomImageName1 = Math.floor(Math.random() * 6 + 1);
-        var randomImageName2 = Math.floor(Math.random() * 6 + 1);
+        let randomImageName1 = Math.floor(Math.random() * 6 + 1);
+        let randomImageName2 = Math.floor(Math.random() * 6 + 1);
 
 
         //Javascript to make Dice game logic
-        var randomImageSource1 = "images/dice"+randomImageName1+".png";
+        let randomImageSource1 = "images/dice"+randomImageName1+".png";
         
-        var randomImageSource2 = "images/dice"+randomImageName2+".png";
+        let randomImageSource2 = "images/dice"+randomImageName2+".png";
         
         document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
         
@@ -22,15 +28,15 @@ document.querySelector(".btn").addEventListener
 
             // If player one wins
             if (randomImageName1 > randomImageName2) {
-                document.querySelector("h1").innerHTML = "You win!";
+                document.querySelector("h1").innerText = "You win!";
             }
             // If player two winsAC
             else if (randomImageName1 < randomImageName2) {
-                document.querySelector("h1").innerHTML = "I win!";
+                document.querySelector("h1").innerText = "I win!";
             }
             // If player one and two tie
             else {
-                document.querySelector("h1").innerHTML = "Tie!";
+                document.querySelector("h1").innerText = "Tie!";
             }
     }
 );
